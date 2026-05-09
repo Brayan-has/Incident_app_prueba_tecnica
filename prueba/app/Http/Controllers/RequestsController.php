1f1a32284c436168e5b2d114365eb952163c9ce7
Nerhon <g.nayarb112@gmail.com>
2026-05-08 19:13:48 -0500

    feat: relationship between user and Request one to many

--- prueba/app/Http/Controllers/RequestsController.php
+++ prueba/app/Http/Controllers/RequestsController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Requests;
+use App\Models\Request as RequestsModel;
 use Illuminate\Http\Request;
+
 
 class RequestsController extends Controller
 {
